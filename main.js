0465f5a44879bb22d7c596e0e4c430713f38ab3d
unknown <abi62176@gmail.com>
2019-03-19 00:09:39 -0700

    First Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+(function() {
+'use strict'
+    
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+// TODO
+paper.view.draw();
+    
+// your code will start here...
+console.log('main.js loaded');
+// ...and end here
+}())
